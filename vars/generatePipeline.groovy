70b377f8d42a88ebbeb4d14296e0895fb3fc4894
hcylus <hercylus@gmail.com>
2020-03-19 18:28:37 +0800

    Create generatePipeline.groovy

---
+++ vars/generatePipeline.groovy
@@ -0,0 +1,49 @@
+def call(String lang) {
+  if (lang == 'go') {
+    pipeline {
+      agent any
+      environment {
+       _version = createVersion()
+      }
+       stages {
+         stage ('build') {
+            steps {
+                script {
+                    def util = new com.hcylus.Utils()
+                    def version = util.createVersion("${BUILD_NUMBER}")
+                    echo "${version}"
+                    sayHello lang
+                    echo "${_version}"
+                }  
+            }
+         }
+       }
+    } 
+  } else if (lang == 'java') {
+    pipeline {
+      agent any
+      environment {
+       _version = createVersion()
+      }
+       stages {
+         stage ('build') {
+            steps {
+              script {
+                    def util = new com.hcylus.Utils()
+                    def version = util.createVersion("${BUILD_NUMBER}")
+                    echo "${version}"
+                    sayHello lang
+                    echo "${_version}"
+                }  
+            }
+         }
+       }
+    } 
+  }
+  // 其他语言
+}
+
+def createVersion() {
+    return new Date().format('yyyyMM') + "-${env.BUILD_NUMBER}"
+    
+}
