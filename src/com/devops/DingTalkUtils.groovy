97fa25f19bc85abb1cf40b23a4f87ae834d19a35
hcylus <hercylus@gmail.com>
2020-05-21 17:53:49 +0800

    Update DingTalkUtils.groovy

--- src/com/devops/DingTalkUtils.groovy
+++ src/com/devops/DingTalkUtils.groovy
@@ -3,7 +3,7 @@ package com.devops
 // 实现 Serializable 接口是为了确保当 pipeline 被 Jenkins挂起后能正确恢复
 class DingTalkUtils implements Serializable {
 
-    url = 'https://oapi.dingtalk.com'
+    String url = 'https://oapi.dingtalk.com'
         
     // 获取构建者uid，依赖build-user-vars-plugin
     def getSubmitter() {
@@ -16,7 +16,7 @@ class DingTalkUtils implements Serializable {
     
     def ApiUrl(api) {
         withCredentials([string(credentialsId: '3ad1ec23-3f99-4f11-9c9f-db5f7a68f069', variable: 'secret'), string(credentialsId: 'f2387bc9-8f0f-4da5-b04a-62d0db6ed1f7', variable: 'key')]) {
-                apiurl = url + api + '?' + 'appkey=' + key +'&' + 'appsecret=' + secret
+                apiurl = this.url + api + '?' + 'appkey=' + key +'&' + 'appsecret=' + secret
                 echo apiurl
                 req = httpRequest contentType: 'APPLICATION_JSON_UTF8', responseHandle: 'NONE', url: apiurl
         }        
@@ -24,6 +24,6 @@ class DingTalkUtils implements Serializable {
     }
     
     def getAccessToken(){
-        return ApiUrl('/gettoken') 
+        return this.ApiUrl('/gettoken') 
     }
 }
