aacd70be5b492b0643f98fe07d344cd6565704b1
hcylus <hercylus@gmail.com>
2020-05-21 16:37:32 +0800

    Update generatePipeline.groovy

--- vars/generatePipeline.groovy
+++ vars/generatePipeline.groovy
@@ -1,32 +1,31 @@
 def call(String lang) {
-    pipeline {
-      agent any
-      environment {
-       _version = createVersion()
+  pipeline {
+    agent any
+    environment {
+      _version = createVersion()
+    }
+    stages {
+      stage ('build') {
+        steps {
+          script {
+            def util = new com.devops.Utils()
+            def version = util.createVersion("${BUILD_NUMBER}")
+            echo "${version}"
+            if (lang == 'go') {
+              echo 'go build'
+              sayHello lang
+            } else if (lang == 'java') { 
+              echo 'java build'
+              sayHello lang 
+            }  
+            echo "${_version}"
+          }  
+        }
       }
-       stages {
-         stage ('build') {
-            steps {
-                script {
-                    def util = new com.devops.Utils()
-                    def version = util.createVersion("${BUILD_NUMBER}")
-                    echo "${version}"
-                    if (lang == 'go') {
-                      echo 'go build'
-                      sayHello lang
-                    } else if (lang == 'java') { 
-                      echo 'java build'
-                      sayHello lang 
-                    }  
-                    echo "${_version}"
-                }  
-            }
-         }
-       }
-    } 
+    }
+  } 
 }
 
 def createVersion() {
-    return new Date().format('yyyyMM') + "-${env.BUILD_NUMBER}"
-    
+  return new Date().format('yyyyMM') + "-${env.BUILD_NUMBER}"
 }
