1a3e08711785b839a11d59e07a5c6bd3b95ff0af
hcylus <hercylus@gmail.com>
2020-05-21 16:42:24 +0800

    Update Utils.groovy

--- src/com/devops/Utils.groovy
+++ src/com/devops/Utils.groovy
@@ -3,6 +3,6 @@ package com.devops
 // 实现 Serializable 接口是为了确保当 pipeline 被 Jenkins挂起后能正确恢复
 class Utils implements Serializable {
 	def createVersion(String BUILD_NUMBER) {
-		return new Date().format('yyyyMM') + "-${BUILD_NUMBER}"
+		return new Date().format('YYYYMMddHHmmss') + "-${BUILD_NUMBER}"
 	}
 }
